30fdd87902fbd6388a7d270f33a3f51926cf55ec
pArtour <edvardi72@gmail.com>
2020-02-22 09:01:35 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,55 @@
+//localStorage
+let local = document.querySelector('.local-block'),
+  textArea = local.querySelector('#local-text'),
+  localNav = local.querySelector('.local-nav');
+  window.onload = saveHistory(textArea); //init saveHistory func on window's load
+
+function saveHistory(elem) {//save chenges of text in textarea into the localStorage
+  var key = elem.tagName.toLowerCase();
+  if (localStorage[key]) { // if key exists on localStorage
+    showButton(key, elem);
+    elem.value = JSON.parse(localStorage[key]).pop();//display last item (history) of elem in LS on textarea
+  }
+  elem.addEventListener('input', setHistory(key, elem));
+}
+
+function setHistory(key, elem) { //set history to the ls
+  var arr = [];
+  return function () {
+    arr.push(elem.value);
+    localStorage.setItem(key, JSON.stringify(arr));
+    showButton(key, elem);
+  };
+}
+
+function showButton(key, elem) { //display buttons func
+  let buttonPrev = elem.parentElement.querySelector('#prev'),
+      buttonNext = elem.parentElement.querySelector('#next');
+
+  addActiveClass(buttonPrev, buttonNext);
+  buttonPrev.onclick = prevValue(key, elem);
+  buttonNext.onclick = nextValue(key, elem);
+}
+
+function nextValue(key, elem) {
+  return function () {
+    let arrValue = JSON.parse(localStorage[key]);
+    var index = arrValue.indexOf(elem.value);
+    if (index < arrValue.length - 1) elem.value = arrValue[++index];
+  };
+}
+
+function prevValue(key, elem) {
+  return function () {
+    let arrValue = JSON.parse(localStorage[key]);
+    arrValue.forEach(function (value, i) {
+      if (value === elem.value && i > 0) elem.value = arrValue[--i];
+    });
+  };
+}
+
+function addActiveClass() {
+  for (let i = 0; i < arguments.length; i++) {
+    if (!arguments[i].classList.contains('active')) arguments[i].classList.add('active');
+  }
+}
